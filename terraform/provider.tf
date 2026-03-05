42709612dfdb6db7b7b484e4b7d967d0d766a0e7
kbaks9 <kamaal.bakar@outlook.com>
2026-03-05 20:56:28 +0000

    feat(terraform): add Azure Storage remote backend to provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.12.2"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sataskmanagertfstate"
+    container_name       = "tfstate"
+    key                  = "taskmanager.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
