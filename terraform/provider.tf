374a9f794cc8a9e985bc01cd9f7aeec16c4e742e
kbaks9 <kamaal.bakar@outlook.com>
2026-06-19 20:10:26 +0100

    fix: move backend config to backend.tf, remove duplicate from provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.12.2"
 
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sataskmanagertfstate"
-    container_name       = "tfstate"
-    key                  = "taskmanager.terraform.tfstate"
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
